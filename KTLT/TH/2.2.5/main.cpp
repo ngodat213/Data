10fe40d0f256d854b4e94b9cd5b3372e7d32c18f
HydraCoder <ngodat.it213@gmail.com>
2022-06-02 10:39:09 +0700

    Update 2.2.5

--- KTLT/TH/2.2.5/main.cpp
+++ KTLT/TH/2.2.5/main.cpp
@@ -6,8 +6,8 @@
 using ll = long long;
 
 struct Date{
-    unsigned char date;
-    unsigned char month;
+    int date;
+    int month;
     int year;
 };
 
@@ -18,24 +18,65 @@ typedef struct Student{
     char stGender;
     char stClass[7];
     float stScore;
-};
+}SV;
 
 // Call function
 void SetDate(Date &date);
 void GetDate(Date date);
 void SetStudent(Student &st);
-void SetStudents(Student st[], int n);
+void SetStudent_Arr(Student st[], int &n);
 void GetStudent(Student st);
-void GetStudents(Student st[], int n);
+void GetStudent_Arr(Student st[], int n);
 void StudentIT(Student st[], int n);
+void StudentScoreThanN(Student st[], int n, int index);
+int CountFemale(Student st[], int n);
+void StudentMaxScore(Student st[], int n);
+void InsertStudent(Student st[], int &n);
+void FindAndDelete(Student st[], int &n, char index[]);
+void SortStudent(Student st[], int n);
 
 int main()
 {
-    Student dataStudent[MAX];
-    SetStudents(dataStudent, 3);
-    StudentIT(dataStudent, 3);
+    int n; char strCmp[31];
+    SV dataStudent[MAX] = {
+        {"001", "Ngo Van Tien Dat", {26,10,2003}, 'y', "21DTHC5", 10.0},
+        {"002", "Vo Thanh am", {12,12,2002}, 'y', "21DTHC5", 1.0},
+        {"003", "Hello word", {26,10,2003}, 'y', "21DTHC5", 10.0},
+        {"004", "Ngo Van Tien Dat", {26,10,2003}, 'y', "21DTHC5", 10.0},
+        {"005", "Ngo Van Tien Dat", {26,10,2003}, 'y', "21DTHC5", 10.0},
+        {"006", "Ngo Van Tien Dat", {26,10,2003}, 'y', "21DTHC5", 10.0},
+        {"007", "Ngo Van Tien Dat", {26,10,2003}, 'y', "21DTHC5", 10.0},
+        {"008", "Ngo Van Tien Dat", {26,10,2003}, 'y', "21DTHC5", 10.0},
+    };
+    SetStudent_Arr(dataStudent, n);
+    GetStudent_Arr(dataStudent, n);
+    StudentScoreThanN(dataStudent, n, 5);
+    StudentIT(dataStudent, n);
+    printf("So sinh vien nu: %d", CountFemale(dataStudent, n));
+    printf("So sinh vien diem cao nhat:\n");
+    StudentMaxScore(dataStudent, n);
+    printf("Them 1 sinh vien: \n");
+    InsertStudent(dataStudent, n);
+    printf("Nhap ten 1 sinh vien X va xoa: \n");
+    fflush(stdin);
+    gets(strCmp);
+    FindAndDelete(dataStudent, n, strCmp);
+    printf("Sap xep sinh vien theo diem trung binh tang dan: \n");
+    SortStudent(dataStudent, n);
+    GetStudent_Arr(dataStudent, n);
     return 0;
 }
+
+void ScanNum(int &n){
+    printf("Nhap so luong san phan: 0 < n <= 100: ");
+    do{
+        scanf("%d", &n);
+        if(n <= 0 || n > 100){
+            printf("Qua so luong quy dinh!\n");
+        }
+    }while(n <= 0 || n > 100);
+}
+
 // Set Date
 void SetDate(Date &date){
     printf("Ngay: ");
@@ -74,11 +115,13 @@ void SetStudent(Student &st){
 }
 
 // Nhap N sinh vien
-void SetStudents(Student st[], int n){
+void SetStudent_Arr(Student st[], int &n){
+    ScanNum(n);
     for(int i = 0; i < n; ++i){
         SetStudent(st[i]);
     }
 }
+
 // Xuat 1 sinh vien
 void GetStudent(Student st){
     printf("\n%-11s\t%-31s\n", st.stId, st.stName);
@@ -88,7 +131,7 @@ void GetStudent(Student st){
     printf("Class: %-8s\nScore: %.1f\n\n", st.stClass, st.stScore);
 }
 // Xuat N sinh vien
-void GetStudents(Student st[], int n){
+void GetStudent_Arr(Student st[], int n){
     for(int i = 0; i < n; ++i){
         GetStudent(st[i]);
     }
@@ -134,12 +177,12 @@ void StudentMaxScore(Student st[], int n){
     }
 }
 
-void AddStudent(Student st[], int &n){
+void InsertStudent(Student st[], int &n){
     ++n;
     SetStudent(st[n]);
 }
 
-void DeleteStudent(Student st[], int &n, int index){
+void RemoveStudent(Student st[], int &n, int index){
     for(int i = index; i < n-1; ++i){
         st[i] = st[i+1];
     }
@@ -149,7 +192,7 @@ void DeleteStudent(Student st[], int &n, int index){
 void FindAndDelete(Student st[], int &n, char index[]){
     for(int i = 0; i < n; ++i){
         if(strcmp(index, st[i].stId) == 0){
-            DeleteStudent(st[], n, i);
+            RemoveStudent(st, n, i);
         }
     }
     printf("Find and delete successful!\n");
