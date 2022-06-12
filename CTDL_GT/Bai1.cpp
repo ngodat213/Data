136ac0aec4c950b89e3ccd02b0990398e646c78e
HydraCoder <ngodat.it213@gmail.com>
2022-06-13 02:51:38 +0700

    Single Linked List

--- CTDL_GT/Bai1.cpp
+++ CTDL_GT/Bai1.cpp
@@ -7,32 +7,32 @@ struct Node{
     Node *next;
 };
 
-int main(){
-    Node *head = new Node();
-    head->data = 1;
-    head->next = NULL;
-
-    Node *second = new Node();
-    second->data = 2;
-    second->next = NULL;
-
-    Node *third = new Node();
-    third->data = 3;
-    third->next = NULL;
-
-    // Lien ket cac node lai voi nhau
-    head->next = second;
-    second->next = third;
-    // truy cap
-    int data = head->data;
-    cout << data << endl;
-
-    Node *p = head;
-    while(p != NULL){
-        data = p->data;
-        p = p->next;
-        cout << data << " -> ";
-
-    }
-    return 0;
-}
+//int main(){
+//    Node *head = new Node();
+//    head->data = 1;
+//    head->next = NULL;
+//
+//    Node *second = new Node();
+//    second->data = 2;
+//    second->next = NULL;
+//
+//    Node *third = new Node();
+//    third->data = 3;
+//    third->next = NULL;
+//
+//    // Lien ket cac node lai voi nhau
+//    head->next = second;
+//    second->next = third;
+//    // truy cap
+//    int data = head->data;
+//    cout << data << endl;
+//
+//    Node *p = head;
+//    while(p != NULL){
+//        data = p->data;
+//        p = p->next;
+//        cout << data << " -> ";
+//
+//    }
+//    return 0;
+//}
