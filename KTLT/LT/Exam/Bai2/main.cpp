2f986c13d94fb556601c1acfde279dfe862d4060
HydraCoder <ngodat.it213@gmail.com>
2022-06-13 02:13:56 +0700

    Update Bai2 MaTran2

--- KTLT/LT/Exam/Bai2/main.cpp
+++ KTLT/LT/Exam/Bai2/main.cpp
@@ -54,12 +54,12 @@ bool isPrime(int n){
 
 int SoNguyenToCuoiCung(int a[][MAX], int m, int n){
     int p = 0;
-    for(int i = 0; i < m; ++i){
-        for(int j = 0; j < n; ++j){
-            if(isPrime(a[i][j]) == 1) p = a[i][j];
+    for(int i = m-1; i >= 0; --i){
+        for(int j = n-1; j >= 0; --j){
+            if(isPrime(a[i][j]) == 1) return a[i][j];
         }
     }
-    return p;
+    return -1;
 }
 
 int main()
