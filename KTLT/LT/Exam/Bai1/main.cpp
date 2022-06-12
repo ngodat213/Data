445d008720c25c554fc54a0c2e404f4fe93cf854
HydraCoder <ngodat.it213@gmail.com>
2022-06-13 02:13:41 +0700

    Update Bai1 DeQuy

--- KTLT/LT/Exam/Bai1/main.cpp
+++ KTLT/LT/Exam/Bai1/main.cpp
@@ -21,7 +21,7 @@ void XuatMang(int *a, int n){
     XuatMang(a, n-1);
     printf("a[%d]: %d\n", n, *(a+(n-1)));
 }
-// Kiem tra le cach 1
+
 int KiemTraLeC1(int *a, int n){
     if(n == 0) return 1;
     if(*(a+(n-1)) % 2 != 0) return 0;
@@ -35,6 +35,7 @@ int KiemTraLeC2(int *a, int n ,bool b = 1){
     if(*(a+(n-1)) % 2 != 0) return KiemTraLeC2(a, n-1, 0);
     else KiemTraLeC2(a, n-1, 1);
 }
+
 int main(){
     int n;
     NhapSL(&n);
